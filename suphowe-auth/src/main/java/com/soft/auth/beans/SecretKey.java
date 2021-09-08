1c8d77395ebd7f6d9e1268e7d811b67eb748e0c2
suphowe <suphowe@gmail.com>
2021-09-08 13:06:37 +0800

    security add

---
+++ suphowe-auth/src/main/java/com/soft/auth/beans/SecretKey.java
@@ -0,0 +1,25 @@
+package com.soft.auth.beans;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 公钥 私钥 实体类
+ * @author suphowe
+ */
+@Data
+public class SecretKey implements Serializable {
+
+    private static final long serialVersionUID = 7432604817420991603L;
+
+    /**
+     * 私钥 JAVA版本的私钥 JAVA读取pkcs8格式比较方便 转换为pkcs8格式的
+     */
+    private String privateKey;
+
+    /**
+     * 公钥
+     */
+    private String publicKey;
+}
