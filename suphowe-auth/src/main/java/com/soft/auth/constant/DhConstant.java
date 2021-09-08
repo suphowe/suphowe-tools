42875d2a3dc8c1ecb610357bffc8872aeb8756f6
suphowe <suphowe@gmail.com>
2021-09-08 11:12:49 +0800

    dh algorithm add

---
+++ suphowe-auth/src/main/java/com/soft/auth/constant/DhConstant.java
@@ -0,0 +1,25 @@
+package com.soft.auth.constant;
+
+import com.soft.auth.utils.SysUtils;
+
+import java.io.Serializable;
+
+/**
+ * dh 配置参数信息
+ *
+ * @author suphowe
+ */
+public class DhConstant implements Serializable {
+
+    private static final long serialVersionUID = -1009602490891371355L;
+
+    public static final String DH_PAIR_KEY = SysUtils.getProperties("properties/dh", "dh.default.pair.key");
+
+    public static final String DH_PUBLIC_KEY = SysUtils.getProperties("properties/dh", "dh.default.public.key");
+
+    public static final String DH_PRIVATE_KEY = SysUtils.getProperties("properties/dh", "dh.default.private.key");
+
+    public static final String DH_ALGORITHM = SysUtils.getProperties("properties/dh", "dh.default.algorithm");
+
+    public static final int SIZE = Integer.parseInt(SysUtils.getProperties("properties/dh", "dh.default.size"));
+}
